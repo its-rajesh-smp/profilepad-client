d95d8d8d313ef902ccafdad3fb059f641112f7bb
its.rajesh.smp <its.rajeshsmp@gmail.com>
2024-11-10 22:07:36 +0530

    ADD: Added share button confetti

--- src/pages/dashboard/components/ActionBar.tsx
+++ src/pages/dashboard/components/ActionBar.tsx
@@ -1,15 +1,13 @@
 import { Button } from "@/common/components/shadcn/ui/button";
 import { useAppDispatch } from "@/common/hooks/useAppDispatch";
-import { BiBox, BiGrid, BiImage, BiLink, BiText } from "react-icons/bi";
-
-import { useAppSelector } from "@/common/hooks/useAppSelector";
 import { motion } from "framer-motion";
+import { BiBox, BiGrid, BiImage, BiLink, BiText } from "react-icons/bi";
 import { createLayoutAct } from "../action-creators/layout-item.act";
 import ActionButtonWithInput from "./UI/ActionButtonWithInput";
+import ShareButton from "./UI/ShareButton";
 
 function ActionBar() {
   const dispatch = useAppDispatch();
-  const { slug } = useAppSelector((state) => state.authSlice.user);
 
   /**
    * Creates a new dashboard item of a given type.
@@ -19,10 +17,6 @@ function ActionBar() {
     dispatch(createLayoutAct(data));
   };
 
-  const onClickShareBtn = () => {
-    navigator.clipboard.writeText(`${window.location.origin}/${slug}`);
-  };
-
   return (
     <motion.div
       initial={{ opacity: 0 }}
@@ -30,14 +24,7 @@ function ActionBar() {
       transition={{ duration: 1, delay: 2 }}
       className="fixed bottom-5 left-1/2 right-0 flex w-fit translate-x-[-50%] gap-2 rounded-md border bg-white p-2 shadow-md"
     >
-      <Button
-        tooltipText="Share"
-        onClick={onClickShareBtn}
-        variant="default"
-        size="xs"
-      >
-        Share
-      </Button>
+      <ShareButton />
 
       <ActionButtonWithInput
         tooltipText="URL"
---
+++ src/pages/dashboard/components/UI/ShareButton.tsx
@@ -0,0 +1,48 @@
+import { Button } from "@/common/components/shadcn/ui/button";
+import { useAppSelector } from "@/common/hooks/useAppSelector";
+import { useEffect, useRef, useState } from "react";
+import ConfettiExplosion from "react-confetti-explosion";
+import { toast } from "sonner";
+
+const confettiConfig = {
+  force: 0.4,
+  duration: 2200,
+  particleCount: 30,
+  width: 400,
+};
+
+function ShareButton() {
+  const { slug } = useAppSelector((state) => state.authSlice.user);
+  const [showConfetti, setShowConfetti] = useState(false);
+
+  const onClickShareBtn = () => {
+    if (showConfetti === true) return;
+    setShowConfetti(true);
+    navigator.clipboard.writeText(`${window.location.origin}/${slug}`);
+    toast("Link is copied to clipboard");
+  };
+
+  return (
+    <div>
+      {showConfetti && (
+        <ConfettiExplosion
+          onComplete={() => {
+            setShowConfetti(false);
+          }}
+          {...confettiConfig}
+        />
+      )}
+      <Button
+        disabled={showConfetti}
+        tooltipText="Share"
+        onClick={onClickShareBtn}
+        variant="default"
+        size="xs"
+      >
+        Share
+      </Button>
+    </div>
+  );
+}
+
+export default ShareButton;
