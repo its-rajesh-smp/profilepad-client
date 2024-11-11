6634aad91a947c27d91a69016396aeba83c0b3de
its.rajesh.smp <its.rajeshsmp@gmail.com>
2024-11-11 06:06:00 +0530

    FIX: Fixed the grid layout size

--- src/pages/dashboard/constants/dashboard-grid.const.ts
+++ src/pages/dashboard/constants/dashboard-grid.const.ts
@@ -1,3 +1,3 @@
 export const BREAKPOINTS = { lg: 1024 / 2, xs: 0 };
-export const COLS = { lg: 16, xs: 4 };
-export const ROW_HIGHT = 40;
+export const COLS = { lg: 12, xs: 4 };
+export const ROW_HIGHT = 20;
