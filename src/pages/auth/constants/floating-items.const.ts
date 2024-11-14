c7583b3596da51f1ffe7d776e527c496a456e7f1
its.rajesh.smp <its.rajeshsmp@gmail.com>
2024-11-14 18:04:36 +0530

    ADD: Added more floating images

--- src/pages/auth/constants/floating-items.const.ts
+++ src/pages/auth/constants/floating-items.const.ts
@@ -5,4 +5,7 @@ export const authPageFloatingItems = [
   "https://images.unsplash.com/photo-1534528741775-53994a69daeb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80",
   "https://bento.me/images/widgets/linkedin.png",
   "https://bento.me/images/widgets/behance.png",
+  "https://cdn.prod.website-files.com/6335b33630f88833a92915fc/63e91b425b939f59bea307ec_substackwidget2-p-500.png",
+  "https://cdn.prod.website-files.com/6335b33630f88833a92915fc/63ea7d42d96b453e6c24c20f_hero%20buymeacoffee-p-500.png",
+  "https://cdn.prod.website-files.com/6335b33630f88833a92915fc/63ebce23e53ac60a7fa7bd43_hero%20youtube.png",
 ];
