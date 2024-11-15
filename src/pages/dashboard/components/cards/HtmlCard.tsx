5b3e06951eec59e5e860efdcae9155b0a2e3c1ee
its.rajesh.smp <its.rajeshsmp@gmail.com>
2024-11-15 08:26:43 +0530

    ADD: Integrated monaco editor to edit the html

--- src/pages/dashboard/components/cards/HtmlCard.tsx
+++ src/pages/dashboard/components/cards/HtmlCard.tsx
@@ -1,20 +1,32 @@
-import AutoSaveTextField from "@/common/components/UI/AutoSaveTextField";
-import { useAppDispatch } from "@/common/hooks/useAppDispatch";
-import { useState } from "react";
+import { debounce } from "@/common/utils/debounce.util";
+import Editor from "@monaco-editor/react";
+import { useCallback, useState } from "react";
 import { defaultHtmlCardHtmlValue } from "../../constants/grid-item.const";
 import { updateLayoutItem } from "../../services/layout-item.service";
 import { IDashboardCard } from "../../types/dashboard.type";
 import HtmlToggleBtn from "../UI/Toolbars/HtmlToggleBtn";
 
 function HtmlCard({ metadata, id }: IDashboardCard) {
+  const [html, setHtml] = useState(metadata?.html ?? defaultHtmlCardHtmlValue);
   const [isHovered, setIsHovered] = useState(false);
   const [preview, setPreview] = useState(true);
-  const dispatch = useAppDispatch();
 
-  const onChange = async (id: string, data: any) => {
-    try {
-      await updateLayoutItem(id, data);
-    } catch (error) {}
+  const updateNestedField = (path: string, value: any) => {
+    const keys = path.split(".");
+    return keys.reduceRight((acc, key) => ({ [key]: acc }), value);
+  };
+
+  const debouncedUpdateOnDb = useCallback(
+    debounce((value: string) => {
+      let updatedField = updateNestedField("metadata.html", value);
+      updateLayoutItem?.(id, updatedField);
+      setHtml(value);
+    }, 500),
+    [id],
+  );
+
+  const onTextChange = (value: any) => {
+    debouncedUpdateOnDb(value);
   };
 
   return (
@@ -24,7 +36,7 @@ function HtmlCard({ metadata, id }: IDashboardCard) {
       className="h-full w-full"
     >
       {isHovered && (
-        <div className="absolute right-0 top-0 z-10 p-2">
+        <div className="absolute right-0 top-0 z-10">
           <HtmlToggleBtn setPreview={setPreview} preview={preview} />
         </div>
       )}
@@ -33,19 +45,13 @@ function HtmlCard({ metadata, id }: IDashboardCard) {
         <div
           className="h-full w-full"
           dangerouslySetInnerHTML={{
-            __html: metadata?.html ?? defaultHtmlCardHtmlValue,
+            __html: html,
           }}
         />
       )}
 
       {!preview && (
-        <AutoSaveTextField
-          onChange={onChange}
-          id={id}
-          fieldToUpdate="metadata.html"
-        >
-          {metadata?.html ?? defaultHtmlCardHtmlValue}
-        </AutoSaveTextField>
+        <Editor defaultLanguage="html" value={html} onChange={onTextChange} />
       )}
     </div>
   );
