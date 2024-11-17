36977a870683baf7a9c49c94bc5abb8d3aeb4a02
its.rajesh.smp <its.rajeshsmp@gmail.com>
2024-11-17 11:32:24 +0530

    FIX: Fixed the text issue of Empty Card

--- src/pages/dashboard/components/cards/EmptyCard.tsx
+++ src/pages/dashboard/components/cards/EmptyCard.tsx
@@ -3,7 +3,7 @@ import { IDashboardCard } from "../../types/dashboard.type";
 function EmptyCard({}: IDashboardCard) {
   return (
     <div className="flex h-full w-full items-center justify-center">
-      <span className="opacity-10">
+      <span className="text-center opacity-10">
         This will not be displayed || used for layout
       </span>
     </div>
