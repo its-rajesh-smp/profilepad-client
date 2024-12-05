1ee8281914b131f5fb3d4e073a431e11c70fd156
its.rajesh.smp <its.rajeshsmp@gmail.com>
2024-12-05 08:04:08 +0530

    ADD: changed the appenv

--- src/common/constants/app.const.ts
+++ src/common/constants/app.const.ts
@@ -1,7 +1,7 @@
 export const APP_NAME = "Dashboard";
 export const APP_ICON =
   "https://cdn-icons-png.flaticon.com/512/8022/8022077.png";
-export const APP_ENV: string = "Development";
+export const APP_ENV: string = "Production";
 export const API_URL =
   APP_ENV == "Production"
     ? "profilepad-server.vercel.app"
