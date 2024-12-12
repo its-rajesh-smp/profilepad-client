4cdff7b9c535aec1fd64bf8947ddf7df0b4ec63c
its.rajesh.smp <its.rajeshsmp@gmail.com>
2024-12-12 18:53:43 +0530

    .

--- src/common/constants/app.const.ts
+++ src/common/constants/app.const.ts
@@ -1,6 +1,5 @@
 export const APP_NAME = "Dashboard";
 export const APP_ICON =
   "https://cdn-icons-png.flaticon.com/512/8022/8022077.png";
-export const APP_ENV: string = "Development";
-// export const API_URL: string = "https://api.itsrajesh.online";
-export const API_URL: string = "http://localhost:3000";
+export const APP_ENV: string = import.meta.env.VITE_APP_ENV;
+export const API_URL: string = import.meta.env.VITE_API_URL;
