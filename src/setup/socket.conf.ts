61bfc45eaf251562f1f74038b05137f189cba6d2
its.rajesh.smp <its.rajeshsmp@gmail.com>
2024-12-12 18:32:49 +0530

    REMOVE: Removed the game files completely

--- src/setup/socket.conf.ts
+++
@@ -1,9 +0,0 @@
-import { API_URL } from "@/common/constants/app.const";
-import { io } from "socket.io-client";
-const socket = io(API_URL);
-
-export const emitEvent = (event: string, data: any) => {
-  socket.emit(event, data);
-};
-
-export default socket;
