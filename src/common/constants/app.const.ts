83eecc559640a77a0630447c911b672bed368b93
its.rajesh.smp <its.rajeshsmp@gmail.com>
2024-12-12 18:54:12 +0530

    .

--- src/common/constants/app.const.ts
+++ src/common/constants/app.const.ts
@@ -1,4 +1,3 @@
-export const APP_NAME = "Dashboard";
 export const APP_ICON =
   "https://cdn-icons-png.flaticon.com/512/8022/8022077.png";
 export const APP_ENV: string = import.meta.env.VITE_APP_ENV;
