51becdd7f969b1b67dc946ce7ebe52505d94a6a9
its.rajesh.smp <its.rajeshsmp@gmail.com>
2025-02-25 22:40:51 +0530

    .

--- src/common/utils/browser.util.ts
+++ src/common/utils/browser.util.ts
@@ -15,7 +15,7 @@ export const getSubdomain = () => {
   const host = window.location.hostname;
   const parts = host.split(".");
   if (parts.length > 2) {
-    return parts[0];
+    // return parts[0];
   }
   return null;
 };
