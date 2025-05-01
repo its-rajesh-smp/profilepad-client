481a6c13557fe4149221abfb4ddd86228bd65aeb
its.rajesh.smp <its.rajeshsmp@gmail.com>
2025-05-01 20:56:29 +0530

    FIX: Fixed the expired URLs

--- src/common/constants/app.const.ts
+++ src/common/constants/app.const.ts
@@ -2,3 +2,5 @@ export const APP_ICON =
   "https://cdn-icons-png.flaticon.com/512/8022/8022077.png";
 export const APP_ENV: string = import.meta.env.VITE_APP_ENV;
 export const API_URL: string = import.meta.env.VITE_API_URL;
+export const GOOGLE_OAUTH_CLIENT_ID: string = import.meta.env
+  .VITE_GOOGLE_OAUTH_CLIENT_ID;
--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,14 @@ import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import App from "./app/App";
 import { Toaster } from "./common/components/shadcn/ui/sonner";
+import { GOOGLE_OAUTH_CLIENT_ID } from "./common/constants/app.const";
 import "./index.css";
 import reduxStore from "./store/store";
 
 createRoot(document.getElementById("root")!).render(
   // <StrictMode>
   <Provider store={reduxStore}>
-    <GoogleOAuthProvider clientId="144611473759-6ck46mv7p5cptj41cg1lo8rd1lr2c8m9.apps.googleusercontent.com">
+    <GoogleOAuthProvider clientId={GOOGLE_OAUTH_CLIENT_ID}>
       <App />
     </GoogleOAuthProvider>
     <Toaster />
